9f91c43f94041b64986ebaa32f036ee2f23d912e
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-18 22:19:06 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
-def my_each (array) 
+def my_each (array) {puts item}
   i = 0 
   
   while i < array.length 
-   
+   item = array[i]
     
-    yield puts array[i]
+    yield (item)
     
   i +=1 
   end
